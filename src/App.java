f932137a6c87939a4a1183e3f8df42833fc6acac
David Peterson <peterdav000@warren.k12.in.us>
2024-09-26 13:45:07 -0400

    0000

--- src/App.java
+++ src/App.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class App {
+import java.time.LocalDateTime;
+import  java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class App {
+
+
+    public static void main(String[] args) {
+        LocalDateTime birthday = LocalDateTime.of(2008, 7,30, 3, 8, 0 );
+
+        ZoneId timeZone1 = ZoneId.of("America/New_York");
+        ZoneId timeZone2 = ZoneId.of("Europe/Paris");
+
+        ZonedDateTime convertedBirthday = ZonedDateTime.of(birthday, timeZone1);
+        ZonedDateTime birthdayEurope = convertedBirthday.withZoneSameInstant(timeZone2);
+
+        System.out.println(convertedBirthday);
+        System.out.println(birthdayEurope);
+
+
+    }
+
 }
